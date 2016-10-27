f1347da3e6a79dfbbef419ba55e4bf49d3676f77
Neto Deolino <netodeolino@gmail.com>
2016-10-27 14:49:08 -0300

    up

---
+++ app/src/main/java/com/neto/deolino/trabalhoandroid/InviteActivity.java
@@ -0,0 +1,48 @@
+package com.neto.deolino.trabalhoandroid;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.neto.deolino.trabalhoandroid.model.Event;
+import com.neto.deolino.trabalhoandroid.model.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by deolino on 27/10/16.
+ */
+public class InviteActivity extends AppCompatActivity {
+
+    ListView inviteListView;
+    Context context;
+    EditText etSearchUser;
+    User user;
+    int eventID;
+    ProgressBar pbInvite;
+    ArrayList<User> searchResults;
+    ImageButton ib;
+    ViewGroup viewGroup;
+    Event event;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite);
+
+        context = this;
+        inviteListView = (ListView) findViewById(R.id.lvInviteSearch);
+        etSearchUser = (EditText) findViewById(R.id.etSearchUser);
+        eventID = getIntent().getIntExtra("eventID", 0);
+        pbInvite = (ProgressBar) findViewById(R.id.pbInvite);
+        searchResults = new ArrayList<>();
+        viewGroup = (ViewGroup) findViewById(R.id.list_item_person);
+
+        event = new Event();
+    }
+}
---
+++ app/src/main/java/com/neto/deolino/trabalhoandroid/MapsActivity.java
@@ -0,0 +1,47 @@
+package com.neto.deolino.trabalhoandroid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.neto.deolino.trabalhoandroid.model.Event;
+
+/**
+ * Created by deolino on 27/10/16.
+ */
+public class MapsActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+
+    TextView tvDistance;
+    TextView tvDuration;
+
+    Intent intent;
+
+    Event event;
+    Context context;
+    int eventID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        context = this;
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        tvDistance = (TextView) findViewById(R.id.tvDistance);
+        tvDuration = (TextView) findViewById(R.id.tvDuration);
+
+        intent = getIntent();
+
+        eventID = intent.getIntExtra("eventID", 0 );
+        Log.d("MapsActivity", "EvID: " + Integer.toString(eventID));
+
+        event = new Event();
+    }
+}

47882a7eb40b972e395f9ebe8d47e17aef645117
Neto Deolino <netodeolino@gmail.com>
2016-11-04 17:48:48 -0300

    04-11-2016

--- app/src/main/java/com/neto/deolino/trabalhoandroid/CreateAccountActivity.java
+++ app/src/main/java/com/neto/deolino/trabalhoandroid/CreateAccountActivity.java
@@ -3,7 +3,7 @@ package com.neto.deolino.trabalhoandroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -11,29 +11,34 @@ import android.widget.Toast;
  */
 public class CreateAccountActivity extends AppCompatActivity {
 
-    TextView etMail;
-    TextView etName;
-    TextView etPassword;
-    TextView etRepeatPassword;
-
+    EditText etMail;
+    EditText etName;
+    EditText etPassword;
+    EditText etRepeatPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        etMail = (EditText) findViewById(R.id.etCreateMail);
+        etName = (EditText) findViewById(R.id.etCreateName);
+        etPassword = (EditText) findViewById(R.id.etCreatePassword);
+        etRepeatPassword = (EditText) findViewById(R.id.etRepeatPassword);
     }
 
     public void createAccount(View view){
-        /*final String mail = etMail.getText().toString();
+        final String mail = etMail.getText().toString();
         final String name = etName.getText().toString();
         final String password = etPassword.getText().toString();
         final String repeatPassword = etRepeatPassword.getText().toString();
+
         if(mail.isEmpty() || name.isEmpty() || password.isEmpty() || repeatPassword.isEmpty()){
             Toast.makeText(CreateAccountActivity.this, getString(R.string.error_empty_fields), Toast.LENGTH_LONG).show();
         }else{
-            Toast.makeText(CreateAccountActivity.this, "LOGIN REALIZADO COM SUCESSO", Toast.LENGTH_LONG).show();
-        }*/
-        Toast.makeText(CreateAccountActivity.this, "CADASTRO REALIZADO COM SUCESSO", Toast.LENGTH_LONG).show();
-        //Implementação aqui do cadastro
+            Toast.makeText(CreateAccountActivity.this, "CADASTRO REALIZADO COM SUCESSO", Toast.LENGTH_LONG).show();
+            finish();
+            //Implementação aqui do cadastro
+        }
     }
 }

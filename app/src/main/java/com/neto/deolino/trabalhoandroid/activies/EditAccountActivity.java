da91833d7c0ec83057ccae6680e8a84273574a2c
Neto Deolino <netodeolino@gmail.com>
2016-11-24 12:57:08 -0300

    add photo

--- app/src/main/java/com/neto/deolino/trabalhoandroid/activies/EditAccountActivity.java
+++ app/src/main/java/com/neto/deolino/trabalhoandroid/activies/EditAccountActivity.java
@@ -14,6 +14,7 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
